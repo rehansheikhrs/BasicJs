d012814256b0934b98400bcc336a29b175d349a5
Rehan Sheikh <rehansheikhrs08@gmail.com>
2024-10-21 16:49:16 +0530

    Basic of Js is Done

---
+++ 01_Variable.js
@@ -0,0 +1,14 @@
+let accountName = "Rehan Sheikh"
+const accountId = 25554555
+let accountEmail = "rehansheikh87@gmail.com"
+var accountPassword = "123456"
+accountCity = "Hyderabad"
+
+let accountState;
+
+// accountId = 78 // Not be change beacause of Constant = const
+// console.log(accountName);
+// console.log(accountBustnessMailId);
+console.log(accountName);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01_convesion_Method.js
@@ -0,0 +1,22 @@
+// conversion methods
+ // Number
+// let score = 433
+
+
+// console.log(typeof score);
+
+// let valueOfString = Number(score)
+// console.log(typeof valueOfString);
+
+// string
+// let stringVlaue = String (score)
+
+// console.log(typeof stringVlaue); 
+
+// boolean 
+let IsLoggedIn = 1
+
+console.log(typeof IsLoggedIn);
+let booleanIsLoggedIN = Boolean(IsLoggedIn);
+console.log( booleanIsLoggedIN);
+console.log(typeof booleanIsLoggedIN);

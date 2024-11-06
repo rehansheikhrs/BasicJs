d80ef51e1ff8813b650109a587ad7ddba90d6722
Rehan Sheikh <rehansheikhrs08@gmail.com>
2024-11-06 11:46:34 +0530

    Done

---
+++ BasicJs_2/iteration/whileordowhileloop.js
@@ -0,0 +1,26 @@
+//While and do while loop in Javascript
+
+// let index = 1
+
+// while (index <= 10) {
+//     console.log(`The index ${index}`);
+//     index = index + 2
+// }
+
+// Array 
+
+// let myArray = ["IronMan", "Thor", "Hulk", "Superman"];
+// let arr = 0;
+// while (arr < myArray.length) {
+     
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;
+    
+// }
+
+// Do While loop
+let score = 1;
+do {
+    console.log(`The Value of Score ${score}`);
+    score = score +1;
+} while (score <=20);
